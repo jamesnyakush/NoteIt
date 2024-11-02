f379ef499af53fc38d8288bda74666c1c79aa996
James Nyakush <jnyakush99@gmail.com>
2024-11-02 14:12:48 +0300

    First commit

---
+++ app/src/main/java/com/jamesnyakush/note/core/di/AppModule.kt
@@ -0,0 +1,27 @@
+package com.jamesnyakush.note.core.di
+
+import android.app.Application
+import androidx.room.Room
+import com.jamesnyakush.note.core.data.local.NoteDb
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNoteDb(application: Application): NoteDb {
+        return Room.databaseBuilder(
+            application,
+            NoteDb::class.java,
+            "note_db.db"
+        ).build()
+    }
+
+}
